af969a23d564e7b2b6c7319ddfd4c4a9d9d13a56
Segev Finer <segev208@gmail.com>
2021-10-23 20:39:53 +0300

    Add findalldevs and related classes

--- cpcap.pxd
+++ cpcap.pxd
@@ -1,3 +1,28 @@
+cdef extern from *:
+    """
+    #ifdef _WIN32
+        #include <WinSock2.h>
+    #else
+        #include <sys/socket.h>
+        #include <netinet/in.h>
+    #endif
+    """
+    enum:
+        AF_INET
+
+    struct sockaddr:
+        unsigned short sa_family
+        char sa_data[14]
+
+    struct in_addr:
+        unsigned long s_addr
+
+    struct sockaddr_in:
+        unsigned short sin_family
+        unsigned short sin_port
+        in_addr sin_addr
+        char sin_zero[8]
+
 cdef extern from "<pcap/pcap.h>":
     enum:
         PCAP_ERRBUF_SIZE
@@ -6,6 +31,39 @@ cdef extern from "<pcap/pcap.h>":
         PCAP_CHAR_ENC_LOCAL
         PCAP_CHAR_ENC_UTF_8
 
+    ctypedef unsigned int bpf_u_int32
+
+    struct pcap_if:
+        pcap_if* next
+        char* name
+        char* description
+        pcap_addr* addresses
+        bpf_u_int32 flags
+
+    enum:
+        PCAP_IF_LOOPBACK
+        PCAP_IF_UP
+        PCAP_IF_RUNNING
+        PCAP_IF_WIRELESS
+        PCAP_IF_CONNECTION_STATUS
+        PCAP_IF_CONNECTION_STATUS_UNKNOWN
+        PCAP_IF_CONNECTION_STATUS_CONNECTED
+        PCAP_IF_CONNECTION_STATUS_DISCONNECTED
+        PCAP_IF_CONNECTION_STATUS_NOT_APPLICABLE
+
+    ctypedef pcap_if pcap_if_t
+
+    struct pcap_addr:
+        pcap_addr* next
+        sockaddr* addr
+        sockaddr* netmask
+        sockaddr* broadaddr
+        sockaddr* dstaddr
+
     int pcap_init(unsigned int, char*)
+
+    int	pcap_findalldevs(pcap_if_t **, char *)
+
+    void pcap_freealldevs(pcap_if_t *)
 
     const char* pcap_lib_version()
--- cypcap.pyx
+++ cypcap.pyx
@@ -1,5 +1,7 @@
 # cython: language_level=3str
 
+import socket
+import enum
 cimport cpcap
 
 
@@ -13,10 +15,97 @@ class error(Exception):
         self.msg = msg
 
 
-cdef char errbuf[cpcap.PCAP_ERRBUF_SIZE]
-if cpcap.pcap_init(cpcap.PCAP_CHAR_ENC_UTF_8, errbuf) < 0:
-    raise error(-1, errbuf)
+cdef char init_errbuf[cpcap.PCAP_ERRBUF_SIZE]
+if cpcap.pcap_init(cpcap.PCAP_CHAR_ENC_UTF_8, init_errbuf) < 0:
+    raise error(-1, init_errbuf)
 
+
+class PcapIf:
+    def __init__(self, name, description, addresses, flags):
+        self.name = name
+        self.description = description
+        self.addresses = addresses
+        self.flags = flags
+
+    def __repr__(self):
+        return f"<PcapIf(name={self.name!r}, description={self.description!r})>"
+
+
+cdef object PcapIf_from_c(cpcap.pcap_if_t* dev):
+    cdef cpcap.pcap_addr* addr
+
+    addresses = []
+    addr = dev.addresses
+    while addr:
+        addresses.append(PcapAddr_from_c(addr))
+        addr = addr.next
+
+    return PcapIf(
+        dev.name.decode(),
+        dev.description.decode(),
+        addresses,
+        PcapIfFlags(dev.flags),
+    )
+
+
+class PcapIfFlags(enum.IntFlag):
+    LOOPBACK = cpcap.PCAP_IF_LOOPBACK
+    UP = cpcap.PCAP_IF_UP
+    RUNNING = cpcap.PCAP_IF_RUNNING
+    WIRELESS = cpcap.PCAP_IF_WIRELESS
+    CONNECTION_STATUS = cpcap.PCAP_IF_CONNECTION_STATUS
+    CONNECTION_STATUS_UNKNOWN = cpcap.PCAP_IF_CONNECTION_STATUS_UNKNOWN
+    CONNECTION_STATUS_CONNECTED = cpcap.PCAP_IF_CONNECTION_STATUS_CONNECTED
+    CONNECTION_STATUS_DISCONNECTED = cpcap.PCAP_IF_CONNECTION_STATUS_DISCONNECTED
+    CONNECTION_STATUS_NOT_APPLICABLE = cpcap.PCAP_IF_CONNECTION_STATUS_NOT_APPLICABLE
+
+
+class PcapAddr:
+    def __init__(self, addr, netmask, broadaddr, dstaddr):
+        self.addr = addr
+        self.netmask = netmask
+        self.broadaddr = broadaddr
+        self.dstaddr = dstaddr
+
+
+cdef object PcapAddr_from_c(cpcap.pcap_addr* addr):
+    return PcapAddr(
+        makesockaddr_addr(addr.addr),
+        makesockaddr_addr(addr.netmask),
+        makesockaddr_addr(addr.broadaddr),
+        makesockaddr_addr(addr.dstaddr),
+    )
+
+
+cdef object makesockaddr_addr(cpcap.sockaddr* addr):
+    if not addr:
+        return None
+    elif addr.sa_family == cpcap.AF_INET:
+        # TODO Barf... Anything less digusting?
+        return socket.inet_ntop(socket.AF_INET, (<unsigned char*>(&(<cpcap.sockaddr_in*>addr).sin_addr.s_addr))[:4])
+    else:
+        return (<unsigned char*>addr)[:sizeof(cpcap.sockaddr)]
+
+
+def findalldevs():
+    cdef char errbuf[cpcap.PCAP_ERRBUF_SIZE]
+    cdef cpcap.pcap_if_t* devs
+    cdef cpcap.pcap_if_t* dev
+
+    if cpcap.pcap_findalldevs(&devs, errbuf) < 0:
+        raise error(-1, errbuf)
+
+    try:
+        result = []
+        dev = devs
+        while dev:
+            result.append(PcapIf_from_c(dev))
+            dev = dev.next
+
+        return result
+    finally:
+        cpcap.pcap_freealldevs(devs)
 
 def lib_version():
+    """Get the version information for libpcap."""
     return cpcap.pcap_lib_version().decode()
