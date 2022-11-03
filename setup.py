7dbec06965b768742d5b7a1b0d91fb29399a9170
Segev Finer <segev208@gmail.com>
2022-11-03 23:46:59 +0200

    Support Python 3.11

--- setup.py
+++ setup.py
@@ -192,6 +192,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="libpcap pcap",
     zip_safe=False,
