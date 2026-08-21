bebdc78a5fd699cf9663b2d82dbec593f961d451
Segev Finer <segev208@gmail.com>
2026-08-21 13:00:18 +0300

    Bumps

--- setup.py
+++ setup.py
@@ -194,6 +194,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     keywords="libpcap pcap",
     zip_safe=False,
